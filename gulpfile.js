48a4204101506fa0e7ab90f64b61136d0413f8bb
Victoria Kariolic <victoria.kariolic@gmail.com>
2017-01-17 00:24:51 -0500

    Dev Environment Setup: add tasks for gulp-plumber, browser-sync, and nodemon; set up appropriate watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,49 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var browsersync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default',['sass','prefix'], function(){
+
+gulp.task('default',['styles', 'server'], function(){
 	console.log("Gulp Task working");
-	gulp.watch('public/css/style.css',['prefix']);
-	gulp.watch('public/scss/style.scss', ['sass']);
+	gulp.watch('public/scss/**/*.scss', ['styles']);
+	gulp.watch(['public/**/*.js','public/**/*.html'], browsersync.reload);
 });
  
-gulp.task('sass', function () {
+gulp.task('styles', function () {
   return gulp.src('public/scss/style.scss')
+    .pipe(plumber(function(error){
+    	console.log('Shit happened');
+    	this.emit("end");
+    }))
     .pipe(sass({
     	outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('public/css'));
+    }))
+    .pipe(autoprefixer({
+        browsers: ['> 1%', 'last 3 versions', 'Firefox >= 20', 'iOS >=7'],
+        cascade: false
+    }))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browsersync.stream());
 });
 
-gulp.task('prefix', function () {
-    gulp.src('public/css/style.css')
-        .pipe(autoprefixer({
-            browsers: ['> 1%', 'last 3 versions', 'Firefox >= 20', 'iOS >=7'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('public/css'))
+gulp.task('server',function(){
+	browsersync.init({
+		proxy: 'localhost:3000',
+		port: 4000
+	});
+	return nodemon({
+		ext: 'js ejs json',
+		ignore: ['public/*']
+	})
+	.on('restart', browsersync.reload);
 });
+
+// gulp.task('prefix', function () {
+//     gulp.src('public/css/style.css')
+
+//         .pipe(gulp.dest('public/css'))
+// });
